afaf3daf1f35b6a7332ce9a5090e1337dc50ec3e
Mewlips <mewlips@gmail.com>
2013-12-24 16:36:56 +0900

    rust-ffmpeg: show licenses

--- src/rust-ffmpeg/main.rs
+++ src/rust-ffmpeg/main.rs
@@ -12,11 +12,11 @@ pub fn main() {
     let get_micro = |v: uint| {v & 0xff};
     let to_str = |v| {format!("{}.{}.{}", get_major(v), get_minor(v), get_micro(v))};
 
-    println!("avutil     : {}", to_str(avutil::version()));
-    println!("avcodec    : {}", to_str(avcodec::version()));
-    println!("avformat   : {}", to_str(avformat::version()));
-    println!("avdevice   : {}", to_str(avdevice::version()));
-    println!("avfilter   : {}", to_str(avfilter::version()));
-    println!("swresample : {}", to_str(swresample::version()));
-    println!("swscale    : {}", to_str(swscale::version()));
+    println!("avutil     : {} ({})", to_str(avutil::version()), avutil::license());
+    println!("avcodec    : {} ({})", to_str(avcodec::version()), avcodec::license());
+    println!("avformat   : {} ({})", to_str(avformat::version()), avformat::license());
+    println!("avdevice   : {} ({})", to_str(avdevice::version()), avdevice::license());
+    println!("avfilter   : {} ({})", to_str(avfilter::version()), avfilter::license());
+    println!("swresample : {} ({})", to_str(swresample::version()), swresample::license());
+    println!("swscale    : {} ({})", to_str(swscale::version()), swscale::license());
 }
