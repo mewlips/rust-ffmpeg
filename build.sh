af16cd0f5f217d25615793f8f86a0be33b10b8a4
Sébastien Crozet <developer@crozet.re>
2014-06-24 21:55:42 +0200

    Compile rlibs too.

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ for lib in src/avutil52 src/avcodec54 src/avcodec55 \
            src/avformat54 src/avformat55 src/avfilter3 src/avfilter4 \
            src/avdevice54 src/avdevice55 src/swresample0 src/swscale2; do
     echo Building $lib ...
-    rustc -L lib --out-dir lib $lib/lib.rs || exit 1
+    rustc -L lib --crate-type dylib --out-dir lib $lib/lib.rs || exit 1
+    rustc -L lib --crate-type rlib  --out-dir lib $lib/lib.rs || exit 1
 done
 
 echo Building rust-ffmpeg ...
